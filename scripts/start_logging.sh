73636bdd9f4fdd26f5ffc95b60305e27b826b71d
imadcat <hui.young@gmail.com>
2015-06-19 15:40:30 +0800

    disable using ansifilter because it removes too much characters including none ANSI

--- scripts/start_logging.sh
+++ scripts/start_logging.sh
@@ -18,24 +18,26 @@ pipe_pane_ansifilter() {
 pipe_pane_sed_osx() {
 	# Warning, very complex regex ahead.
 	# Some characters below might not be visible from github web view.
-	local ansi_codes_osx="(\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]||]0;[^]+|[[:space:]]+$)"
+	local ansi_codes_osx="(\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]|
+|]0;[^]+|[[:space:]]+$)"
 	tmux pipe-pane "exec cat - | sed -E \"s/$ansi_codes_osx//g\" >> $FILE"
 }
 
 pipe_pane_sed() {
-	local ansi_codes="(\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]|)"
+	local ansi_codes="(\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]|
+)"
 	tmux pipe-pane "exec cat - | sed -r 's/$ansi_codes//g' >> $FILE"
 }
 
 start_pipe_pane() {
-	if ansifilter_installed; then
-		pipe_pane_ansifilter
-	elif system_osx; then
-		# OSX uses sed '-E' flag and a slightly different regex
-		pipe_pane_sed_osx
-	else
+#	if ansifilter_installed; then
+#		pipe_pane_ansifilter
+#	elif system_osx; then
+#		# OSX uses sed '-E' flag and a slightly different regex
+#		pipe_pane_sed_osx
+#	else
 		pipe_pane_sed
-	fi
+#	fi
 }
 
 main() {
